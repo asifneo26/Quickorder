6ae9c076335f2e6add6d1f7985b5d02db71b50a4
asifneo26 <magentodeveloper7600@gmail.com>
2025-07-16 15:52:05 +0530

    new developement

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            quickview: 'Lof_FastOrder/js/quickview',
+            enhancedProductUI: 'Lof_FastOrder/js/enhanced-product-ui',
+        }
+    }
+};
